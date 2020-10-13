c91e8010dc0a726ee6c70a81d690753093f54f0c
Mateo Revilla <mateorevillalecca@gmail.com>
2020-10-13 19:22:45 -0400

    Implementation of Gradle and connection to MongoDB Atlas

---
+++ src/main/java/HellsKitchenRecipeBook/App.java
@@ -0,0 +1,14 @@
+package HellsKitchenRecipeBook;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("RUN");
+
+        Server server = new Server();
+
+
+
+    }
+}
---
+++ src/main/java/HellsKitchenRecipeBook/Server.java
@@ -0,0 +1,117 @@
+package HellsKitchenRecipeBook;
+
+import java.util.*;
+import java.io.*;
+import org.bson.Document;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.MongoClientSettings;
+
+public class Server {
+
+	private MongoCollection collection;
+
+	Server(){
+		ConnectionString connString = new ConnectionString(
+    	"mongodb+srv://user1:hellskitchen@hellskitchenrecipebook.vempc.mongodb.net/HellsKitchenRecipeBook?retryWrites=true&w=majority"
+		);
+		MongoClientSettings settings = MongoClientSettings.builder()
+    	.applyConnectionString(connString)
+    	.retryWrites(true)
+    	.build();
+		MongoClient mongoClient = MongoClients.create(settings);
+		MongoDatabase database = mongoClient.getDatabase("RecipeBook");
+		collection = database.getCollection("Recipes");
+		testInsert();
+
+
+	}
+
+	public void testInsert() {
+		Document document = new Document();
+		document.append("title", "THETITLE");
+		document.append("description", "DESCRIPTION");
+		List<String> ingredients = Arrays.asList("a","b","C");
+		document.append("ingredients", ingredients);
+		List<String> instructions = Arrays.asList("e","d","f");
+		document.append("instructions", instructions);
+		collection.insertOne(document);
+	}
+	
+	// adds recipe to local memory and JSON database
+	public void addRecipeToDatabase(Recipe recipe) {
+	}
+	
+	
+	
+	// prompts user for new recipe information then stores it
+	public void createRecipe(Scanner scanner) {
+		/*System.out.println("Welcome to creating a recipe!");
+		
+		System.out.println("What will the recipe be called?");
+		String name = scanner.nextLine().toLowerCase();
+		System.out.println("Please add a description then press ENTER.");
+		String des = scanner.nextLine();
+		
+		Recipe newRecipe = new Recipe(UUID.randomUUID().toString(), name, des);
+		
+		ArrayList<String> ingredients = newRecipe.getIngredients();
+		System.out.println("Enter the list of ingredients one at a time. Enter 'OK' when done");
+		String ing = scanner.nextLine().toLowerCase();
+		while (!ing.equals("ok")) {
+			ingredients.add(ing);
+			System.out.println("Enter next ingredient or enter 'OK' when done");
+			ing = scanner.nextLine().toLowerCase();
+		}
+		
+		ArrayList<String> instructions = newRecipe.getInstructions();
+		System.out.println("Enter cooking instructions one step at a time. After each step, press ENTER. Enter 'OK' when done");
+		String instr = scanner.nextLine();
+		String instrLowerCase = instr.toLowerCase();
+		int i = 1;
+		while (!instrLowerCase.equals("ok")) {
+			instr = i + ". " + instr;
+			instructions.add(instr);
+			System.out.println("Enter the next step of the cooking instructions then press ENTER or enter 'OK' when done");
+			instr = scanner.nextLine();
+			instrLowerCase = instr.toLowerCase();
+			i++;
+		}
+		
+		System.out.println("Adding the following recipe to recipe book...");
+		newRecipe.displayRecipe();
+		addRecipeToDatabase(newRecipe);*/
+	}
+
+	
+
+
+	public Recipe searchRecipe(String searchTitle) {
+		return null;	
+	}
+
+	public Recipe browseRecipe(Scanner scanner) {
+		/*for (int i = 0; i < this.recipeList.size(); i++) {
+			Recipe rec = this.recipeList.get(i);
+			String recTitle = rec.getTitle();
+			String redId = rec.getId();
+			System.out.println(redId + " " + recTitle);
+		}
+		System.out.println("Please enter the ID number of the recipe you want to select");
+		String id = scanner.nextLine();
+		for (int i = 0; i < this.recipeList.size(); i++) {
+			Recipe rec = this.recipeList.get(i);
+			String redId = rec.getId();
+			if (id.equals(redId)) {
+				return rec;
+			}
+		}
+		//INVALID ID
+		System.out.println("No matches found");*/
+		return null;
+	}
+}
